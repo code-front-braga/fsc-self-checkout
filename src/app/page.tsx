004038ae5c7ab7844aec3e783a5114260c070ab7
Leonardo Braga <programer_frontbraga@outlook.com>
2025-07-06 21:32:47 -0300

    feat: add button to redirect to self checkout app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
-import React from 'react';
+'use client';
 
-const HomePage = async () => {
-	return <div>HomePage</div>;
+import { useRouter } from 'next/navigation';
+
+import { Button } from '@/ui/button';
+
+const HomePage = () => {
+	const router = useRouter();
+
+	return (
+		<div className="flex h-full bg-yellow-400">
+			<Button
+				onClick={() => router.push('/fsw-donalds')}
+				variant="outline"
+				className="m-auto"
+			>
+				Acessar Self Checkout do Mc Donalds
+			</Button>
+		</div>
+	);
 };
 
 export default HomePage;
